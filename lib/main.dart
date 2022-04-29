1fa9208a65b5c90f33931172a11a074c6ec25a36
tryggos16 <tryggvi.bogason@gmail.com>
2022-04-29 19:17:27 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+
+      theme: ThemeData(
+        backgroundColor: Colors.red,
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
+
+
